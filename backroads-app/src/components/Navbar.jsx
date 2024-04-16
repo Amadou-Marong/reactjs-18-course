7d8b76a2735306eb258bf71865c9bb1baaf92b00
Amadou-Marong <marongamadou6@gmail.com>
2024-04-16 15:41:38 +0000

    social links

--- backroads-app/src/components/Navbar.jsx
+++ backroads-app/src/components/Navbar.jsx
@@ -34,34 +34,8 @@ const Navbar = () => {
                   </li>
                 )
             })}
-            {/* <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-facebook"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.twitter.com"
-                target="_blank"
-                className="nav-icon"
-              >
-                <i className="fab fa-squarespace"></i>
-              </a>
-            </li> */}
           </ul>
+          
         </div>
       </nav>
     );
