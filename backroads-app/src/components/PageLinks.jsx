fde03ffb8ba068023543ceeb32ea5bfc8bcab1a7
Amadou-Marong <marongamadou6@gmail.com>
2024-04-18 02:13:17 +0000

    social and page links component updated alternative approach updated

--- backroads-app/src/components/PageLinks.jsx
+++ backroads-app/src/components/PageLinks.jsx
@@ -1,19 +1,18 @@
-// import PageLink from "./PageLink"
-// import { pageLinks } from "./data"
-// const PageLinks = ({parentClass, itemClass}) => {
-//     return (
-//         <ul className={parentClass} id="nav-links">
-//             { pageLinks.map((link) => {
-//                return (
-//                 <PageLink key={link.id} {...link} className=/>
-//                 // <li key={link.id}>
-//                 //   <a href={link.url} className={itemClass}>
-//                 //     {link.text}
-//                 //   </a>
-//                 // </li>
-//                )
-//             })}
-//         </ul>
-//     )
-// }
-// export default PageLinks
+import PageLink from "./PageLink"
+import { pageLinks } from "./data"
+const PageLinks = ({parentClass, itemClass}) => {
+    return (
+        <ul className={parentClass} id="nav-links">
+            { pageLinks.map((link) => {
+               return (
+                <li key={link.id}>
+                  <a href={link.url} className={itemClass}>
+                    {link.text}
+                  </a>
+                </li>
+               )
+            })}
+        </ul>
+    )
+}
+export default PageLinks
