e609a97af38ed98e7b1b72be1cf94e92260704ef
Amadou-Marong <marongamadou6@gmail.com>
2024-04-18 01:24:08 +0000

    social links component updated alternative approach

--- backroads-app/src/components/SocialLinks.jsx
+++ backroads-app/src/components/SocialLinks.jsx
@@ -3,10 +3,11 @@ const SocialLinks = ({ parentClass, itemClass }) => {
   return (
     <ul className={parentClass}>
       {socialLinks.map((link) => {
+        const {id, url, icon} = link
         return (
-          <li key={link.id}>
-            <a href={link.url} target="_blank" className={itemClass}>
-              <i className={link.icon}></i>
+          <li key={id}>
+            <a href={url} target="_blank" className={itemClass}>
+              <i className={icon}></i>
             </a>
           </li>
         );
--- backroads-app/src/components/Tours.jsx
+++ backroads-app/src/components/Tours.jsx
@@ -8,31 +8,8 @@ const Tours = () => {
 
       <div className="section-center featured-center">
         {tours.map((tour) => {
-          // const {id, image, date, title, description, country, days, price} = tour
           return (
             <Tour key={tour.id} {...tour}/>
-            // <article className="tour-card" key={id}>
-            //   <div className="tour-img-container">
-            //     <img src={image} className="tour-img" alt={title} />
-            //     <p className="tour-date">{date}</p>
-            //   </div>
-            //   <div className="tour-info">
-            //     <div className="tour-title">
-            //       <h4>{title}</h4>
-            //     </div>
-            //     <p>{description}</p>
-            //     <div className="tour-footer">
-            //       <p>
-            //         <span>
-            //           <i className="fas fa-map"></i>
-            //         </span>{" "}
-            //         {country}
-            //       </p>
-            //       <p>{`${days} days`}</p>
-            //       <p>{`from $${price}`}</p>
-            //     </div>
-            //   </div>
-            // </article>
           );
         })}
       </div>
