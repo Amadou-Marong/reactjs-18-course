06fa8bd2ae8314cbfbc34a588481801295e6e95e
Amadou-Marong <marongamadou6@gmail.com>
2024-04-18 01:04:42 +0000

    tours component updated alternative approach

--- backroads-app/src/components/Tours.jsx
+++ backroads-app/src/components/Tours.jsx
@@ -1,4 +1,5 @@
 import Title from "./Title";
+import Tour from "./Tour";
 import { tours } from "./data";
 const Tours = () => {
   return (
@@ -7,30 +8,31 @@ const Tours = () => {
 
       <div className="section-center featured-center">
         {tours.map((tour) => {
-          const {id, image, date, title, description, country, days, price} = tour
+          // const {id, image, date, title, description, country, days, price} = tour
           return (
-            <article className="tour-card" key={id}>
-              <div className="tour-img-container">
-                <img src={image} className="tour-img" alt={title} />
-                <p className="tour-date">{date}</p>
-              </div>
-              <div className="tour-info">
-                <div className="tour-title">
-                  <h4>{title}</h4>
-                </div>
-                <p>{description}</p>
-                <div className="tour-footer">
-                  <p>
-                    <span>
-                      <i className="fas fa-map"></i>
-                    </span>{" "}
-                    {country}
-                  </p>
-                  <p>{`${days} days`}</p>
-                  <p>{`from $${price}`}</p>
-                </div>
-              </div>
-            </article>
+            <Tour key={tour.id} {...tour}/>
+            // <article className="tour-card" key={id}>
+            //   <div className="tour-img-container">
+            //     <img src={image} className="tour-img" alt={title} />
+            //     <p className="tour-date">{date}</p>
+            //   </div>
+            //   <div className="tour-info">
+            //     <div className="tour-title">
+            //       <h4>{title}</h4>
+            //     </div>
+            //     <p>{description}</p>
+            //     <div className="tour-footer">
+            //       <p>
+            //         <span>
+            //           <i className="fas fa-map"></i>
+            //         </span>{" "}
+            //         {country}
+            //       </p>
+            //       <p>{`${days} days`}</p>
+            //       <p>{`from $${price}`}</p>
+            //     </div>
+            //   </div>
+            // </article>
           );
         })}
       </div>
