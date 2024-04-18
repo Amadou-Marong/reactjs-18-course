8b6a5520cc243205e55fa4c54edd14d89ad6860d
Amadou-Marong <marongamadou6@gmail.com>
2024-04-18 00:59:56 +0000

    tours component updated

--- backroads-app/src/components/Tours.jsx
+++ backroads-app/src/components/Tours.jsx
@@ -7,26 +7,27 @@ const Tours = () => {
 
       <div className="section-center featured-center">
         {tours.map((tour) => {
+          const {id, image, date, title, description, country, days, price} = tour
           return (
-            <article className="tour-card" key={tour.id}>
+            <article className="tour-card" key={id}>
               <div className="tour-img-container">
-                <img src={tour.image} className="tour-img" alt={tour.title} />
-                <p className="tour-date">{tour.date}</p>
+                <img src={image} className="tour-img" alt={title} />
+                <p className="tour-date">{date}</p>
               </div>
               <div className="tour-info">
                 <div className="tour-title">
-                  <h4>{tour.title}</h4>
+                  <h4>{title}</h4>
                 </div>
-                <p>{tour.title}</p>
+                <p>{description}</p>
                 <div className="tour-footer">
                   <p>
                     <span>
                       <i className="fas fa-map"></i>
                     </span>{" "}
-                    {tour.country}
+                    {country}
                   </p>
-                  <p>{`${tour.days} days`}</p>
-                  <p>{`from $${tour.price}`}</p>
+                  <p>{`${days} days`}</p>
+                  <p>{`from $${price}`}</p>
                 </div>
               </div>
             </article>
