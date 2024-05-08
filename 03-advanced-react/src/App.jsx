d4a22b0187e8ba1ceab4e5f952fefb9897d38eb3
Amadou-Marong <marongamadou6@gmail.com>
2024-05-08 16:10:50 +0000

    custom hooks

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,11 +1,9 @@
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+import Starter from './tutorial/08-custom-hooks/starter/01-toggle'
 // import Final from './tutorial/01-useState/final/03-useState-array'
 function App() {
   return (
     <div className='container'>
-      {/* <h2>Advanced React</h2> */}
       <Starter />
-      {/* <Final /> */}
     </div>
   );
 }
