3d16a9d44d04888f05dfb277b53545f00805706e
Amadou-Marong <marongamadou6@gmail.com>
2024-05-09 15:01:53 +0000

    context api solution

--- 03-advanced-react/src/tutorial/09-context-api/final/context/Navbar.jsx
+++ 03-advanced-react/src/tutorial/09-context-api/final/context/Navbar.jsx
@@ -14,9 +14,11 @@ export const useAppContext = () => useContext(NavbarContext);
 
 const Navbar = () => {
   const [user, setUser] = useState({ name: 'bob' });
+  
   const logout = () => {
     setUser(null);
   };
+
   return (
     <NavbarContext.Provider value={{ user, logout }}>
       <nav className='navbar'>
