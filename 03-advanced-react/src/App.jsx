daf20ad61b131d0da4032c493fc0ebd6d1328401
Amadou-Marong <marongamadou6@gmail.com>
2024-05-20 15:22:08 +0000

    useTransition hook

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/11-performance/starter/03-hooks'
+import Starter from './tutorial/11-performance/starter/04-react-18'
 // import Starter from './tutorial/09-context-api/final/prop-drilling/Navbar'
 // import Final from './tutorial/01-useState/final/03-useState-array'
 function App() {
@@ -7,6 +7,6 @@ function App() {
       <Starter />
     </div>
   );
-}
+} 
 
 export default App;
