4e7b9571aaca4307fd8d2796508ffb3f935a1ba5
Amadou-Marong <marongamadou6@gmail.com>
2024-05-22 11:38:38 +0000

    custom instance

--- 05-axios-tutorial/src/examples/5-custom-instance.js
+++ 05-axios-tutorial/src/examples/5-custom-instance.js
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
-
+import AuthFetch from '../axios/custom';
+import axios from 'axios';
 const randomUserUrl = 'https://randomuser.me/api';
+
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try{
+      const resp1 = await AuthFetch('/react-store-products')
+      console.log(resp1);
+      const resp2 = await axios(randomUserUrl)
+      console.log(resp2);
+    }catch(error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
