6aa3d1bb0f6eb181249339c64231a496e4dab5b0
Amadou-Marong <marongamadou6@gmail.com>
2024-05-25 23:39:21 +0000

    delete task query

--- 06-react-query/starter/src/SingleItem.jsx
+++ 06-react-query/starter/src/SingleItem.jsx
@@ -11,6 +11,15 @@ const SingleItem = ({ item }) => {
       queryClient.invalidateQueries({ queryKey: ['tasks']})
     }
   })
+  
+  const { mutate: deleteTask } = useMutation({
+    mutationFn:(taskId) => {
+      return customFetch.delete(`/${taskId}`)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks']})
+    }
+  })
   return (
     <div className='single-item'>
       <input
@@ -29,7 +38,7 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
