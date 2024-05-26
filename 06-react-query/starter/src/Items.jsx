851fbcba69646ae438bdd5d221bb961f148f5799
Amadou-Marong <marongamadou6@gmail.com>
2024-05-26 07:49:21 +0000

    refactor code

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,29 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
-import customFetch from './utils';
+import { useFetchTasks } from './reactQueryCustomHooks';
 
 const Items = () => {
-  const {isLoading, data, isError, error} = useQuery({
-    queryKey: ['tasks'],
-    queryFn: async () => {
-      const {data} = await customFetch.get('/')
-      return data
-    }
-  })
   
+  const {isLoading, isError, data} = useFetchTasks()  
   if (isLoading) {
     return <div style={{marginTop: '50px'}}>
       <div style={{width: '30px', height: '30px'}} className="loading"></div>
     </div>
   }
 
-  // if (isError) {  
-  //   return <p style={{marginTop: '1rem'}}>There was an error...</p>
-  // }
-
-  if (error) {
-    return <p style={{marginTop: '1rem'}}>{error.response.data}</p>
+  if (isError) {  
+    return <p style={{marginTop: '1rem'}}>There was an error...</p>
   }
+
+  // if (error) {
+  //   return <p style={{marginTop: '1rem'}}>{error.response.data}</p>
+  // }
 
   return (
     <div className='items'>
---
+++ 06-react-query/starter/src/reactQueryCustomHooks.jsx
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+export const useFetchTasks = () => {
+    const {isLoading, data, isError, error} = useQuery({
+        queryKey: ['tasks'],
+        queryFn: async () => {
+          const {data} = await customFetch.get('/')
+          return data
+        }
+    })
+    return {isLoading, isError, data}
+}
