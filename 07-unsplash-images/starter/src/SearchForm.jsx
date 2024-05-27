1d5c92b8765d35e64b2e99909cfb20fbc0f7f1cb
Amadou-Marong <marongamadou6@gmail.com>
2024-05-27 12:56:15 +0000

    search form

--- 07-unsplash-images/starter/src/SearchForm.jsx
+++ 07-unsplash-images/starter/src/SearchForm.jsx
@@ -1,6 +1,20 @@
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if(!searchValue) return;
+    console.log(searchValue);
+  }
   return (
-    <h2>SearchForm</h2>
+    <section>
+      <h1 className="title">unslaph images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" name="search" placeholder="cat" className="form-input" />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
   )
 }
 
