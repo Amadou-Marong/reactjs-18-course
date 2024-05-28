38dbc862f6ecffddb2a60e5f309032fccbf2f9b9
Amadou-Marong <marongamadou6@gmail.com>
2024-05-28 05:26:25 +0000

    display images

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -1,9 +1,46 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+
 const url = 'https://api.unsplash.com/search/photos?client_id=Bpd7ZNo6P1Il7TQPkK1oQLw1Jvz19IYEN2oNgErZRCI&query=office'
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url)
+
+      return result.data
+    }
+  })
+  // console.log(response);
+  if(response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+  
+  const results = response.data.results;
+
+  if(results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No Results Found</h4>
+      </section>
+    )
+  }
+
   return (
-    <h2>Gallery</h2>
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular
+        return <img src={url} key={item.id} alt={item.alt_description} className="img"/>
+      })}
+    </section>
   )
+
+  
 }
 
 export default Gallery
