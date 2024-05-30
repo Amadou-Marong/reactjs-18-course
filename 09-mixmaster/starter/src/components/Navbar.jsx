b1dafcfc03bc58c88f531d9589342d6bb7e9e3e5
Amadou-Marong <marongamadou6@gmail.com>
2024-05-30 13:43:06 +0000

    navbar component

--- 09-mixmaster/starter/src/components/Navbar.jsx
+++ 09-mixmaster/starter/src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
-
+import Wrapper from "../assets/wrappers/Navbar"
 
 const Navbar = () => {
   return (
-    <div className="nav-center">
-        <span className="logo">Coacktail Master</span>
-        <div className="nav-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/about" className="nav-link">About</Link>
-            <Link to="/newsletter" className="nav-link">Newsletter</Link>
+    <Wrapper>
+        <div className="nav-center">
+            <span className="logo">Coacktail Master</span>
+            <div className="nav-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/about" className="nav-link">About</Link>
+                <Link to="/newsletter" className="nav-link">Newsletter</Link>
+            </div>
         </div>
-    </div>
+    </Wrapper>
   )
 }
 
--- 09-mixmaster/starter/src/pages/About.jsx
+++ 09-mixmaster/starter/src/pages/About.jsx
@@ -3,7 +3,6 @@ const About = () => {
   return (
     <div>
         <h2>About</h2>
-        <Link to='/'>Home</Link>
     </div>
   )
 }
