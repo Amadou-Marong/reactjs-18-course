d866933571f6fdf65eb953d0ccd0072a942b95ae
Amadou-Marong <marongamadou6@gmail.com>
2024-06-03 10:53:25 +0000

    single drinks

--- 09-mixmaster/starter/src/pages/Cocktail.jsx
+++ 09-mixmaster/starter/src/pages/Cocktail.jsx
@@ -3,9 +3,26 @@ import axios from "axios"
 import Wrapper from "../assets/wrappers/CocktailPage"
 const singleCocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-export const loader = async (data) => {}
+export const loader = async ({params}) => {
+  const {id} = params
+  const { data } = await axios.get(`${singleCocktailUrl}${id}`)
+ 
+  return {id, data}
+}
 
 const Cocktail = () => {
+  const { id, data } = useLoaderData()
+  
+  const singleDrink = data.drinks[0]
+  const {
+    strAlcoholic: info,
+    strCategory: category,
+    strDrink: name,
+    strDrinkThumb: image,
+    strGlass: glass,
+    strInstructions: instructions
+  } = singleDrink
+
   return (
     <div>
         <h2>Cocktail</h2>
