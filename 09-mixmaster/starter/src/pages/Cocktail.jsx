c06c0ddb53516db52c4b4cdb518ca5b1a3a47888
Amadou-Marong <marongamadou6@gmail.com>
2024-06-03 05:34:25 +0000

    global loading

--- 09-mixmaster/starter/src/pages/Cocktail.jsx
+++ 09-mixmaster/starter/src/pages/Cocktail.jsx
@@ -1,6 +1,9 @@
 import { useLoaderData, Link } from "react-router-dom"
 import axios from "axios"
 import Wrapper from "../assets/wrappers/CocktailPage"
+const singleCocktailUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const loader = async (data) => {}
 
 const Cocktail = () => {
   return (
--- 09-mixmaster/starter/src/pages/HomeLayout.jsx
+++ 09-mixmaster/starter/src/pages/HomeLayout.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import Navbar from "../components/Navbar"
 
 const HomeLayout = () => {
-  return (
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === 'loading'
+
+  return(
     <>
-        <Navbar />
+      <Navbar /> 
         <section className="page">
+          {
+            isPageLoading ? <div className="loading"/> : 
             <Outlet />
+          }
         </section>
-    </>
+    </> 
   )
 }
 
