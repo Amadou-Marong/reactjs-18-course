490975c96702a95a2cc2dd43fb49fac3914ad917
Amadou-Marong <marongamadou6@gmail.com>
2024-06-04 14:27:36 +0000

    newsletter

--- 09-mixmaster/starter/src/pages/Newsletter.jsx
+++ 09-mixmaster/starter/src/pages/Newsletter.jsx
@@ -1,9 +1,24 @@
 
 const Newsletter = () => {
   return (
-    <div>
-        <h2>Newsletter</h2>
-    </div>
+    <section className="page">
+      <form action="" className="form">
+        <h3 style={{marginBottom: "2rem", textAlign: "center"}}>Our Newsletter</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">first name</label>
+          <input type="text" name="name" className="form-input" id="name" defaultValue="imam"/>
+        </div>
+        <div className="form-row">
+          <label htmlFor="lastName" className="form-label">last name</label>
+          <input type="text" name="lastName" className="form-input" id="lastName" defaultValue="ahmad"/>
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">email</label>
+          <input type="text" name="email" className="form-input" id="email" defaultValue="test@test.com"/>
+        </div>
+        <button type="submit" className="btn btn-block">Submit</button>
+      </form>
+    </section>
   )
 }
 
