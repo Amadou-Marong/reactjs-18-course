95b557f145e8e52e85945ec0f209c40d8f72145c
Amadou-Marong <marongamadou6@gmail.com>
2024-06-06 05:30:42 +0000

    newsletter submit form and response

--- 09-mixmaster/starter/src/pages/Newsletter.jsx
+++ 09-mixmaster/starter/src/pages/Newsletter.jsx
@@ -1,10 +1,22 @@
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
 export const action = async ({request}) => {
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
-  console.log(data);
-  return null;
+
+  try {
+    const response = await axios.post(newsletterUrl, data)
+    console.log(response);
+    toast.success(response.data.msg)
+    return redirect('/');
+  } catch (error) {
+    console.log(error);
+  }
+  
 }
 
 const Newsletter = () => {
