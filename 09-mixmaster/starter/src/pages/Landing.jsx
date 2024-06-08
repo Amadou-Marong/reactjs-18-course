bc5919983d0721e9be99e0383e13f54abdd03459
Amadou-Marong <marongamadou6@gmail.com>
2024-06-08 04:27:47 +0000

    react query in landing page

--- 09-mixmaster/starter/src/pages/Landing.jsx
+++ 09-mixmaster/starter/src/pages/Landing.jsx
@@ -5,11 +5,24 @@ import SearchForm from "../components/SearchForm";
 
 const cocktailSearchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+      return response.data.drinks 
+    }
+  }
+}
+
 export const loader = async ({request}) => {
   const url = new URL(request.url)
+  
   const searchTerm = url.searchParams.get('search') || ''
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
-  return { drinks: response.data.drinks, searchTerm }
+
+  return { searchTerm }
 }
 const Landing = () => {
   const { drinks, searchTerm } = useLoaderData()
