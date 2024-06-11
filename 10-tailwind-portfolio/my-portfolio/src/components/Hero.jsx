10a582493062afc4f55d1ff863917121dc7d686f
Amadou-Marong <marongamadou6@gmail.com>
2024-06-11 15:02:44 +0000

    hero component custom tailwindcss class

--- 10-tailwind-portfolio/my-portfolio/src/components/Hero.jsx
+++ 10-tailwind-portfolio/my-portfolio/src/components/Hero.jsx
@@ -1,8 +1,9 @@
+import heroImg from '../assets/hero.svg'
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 const Hero = () => {
     return (
         <section className="bg-emerald-100 py-24">
-            <div className="mx-auto max-w-7xl px-8 py-3 grid md:grid-cols-2 items-center gap-2">
+            <div className="align-element py-3 grid md:grid-cols-2 items-center gap-2">
                 <article>
                     <h1 className="text-7xl font-bold tracking-wider ">I Am Ahmad</h1>
                     <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">Front End Developer</p>
@@ -18,6 +19,9 @@ const Hero = () => {
                             <FaTwitterSquare className="text-4xl text-slate-700 hover:text-emerald-600 duration-300" />
                         </a>
                     </div>
+                </article>
+                <article className=''>
+                    <img src={heroImg} alt="" className="h-80 lg:h-96" />
                 </article>
             </div>
 
