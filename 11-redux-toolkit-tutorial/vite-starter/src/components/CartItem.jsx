8795ccf0e492dd13ec64805ad0135d5226740f1e
Amadou-Marong <marongamadou6@gmail.com>
2024-06-15 20:54:28 +0000

    cartItem

--- 11-redux-toolkit-tutorial/vite-starter/src/components/CartItem.jsx
+++ 11-redux-toolkit-tutorial/vite-starter/src/components/CartItem.jsx
@@ -1,3 +1,5 @@
+import { ChevronDown, ChevronUp } from "../icons"
+
 const CartItem = ({id, img, price, title, amount}) => {
   return (
     <article className="cart-item">
@@ -5,6 +7,16 @@ const CartItem = ({id, img, price, title, amount}) => {
         <div>
             <h4>{title}</h4>
             <h4 className="item-price">${price}</h4>
+            <button className="remove-btn">remove</button>
+        </div>
+        <div>
+            <button className="amount-btn">
+                <ChevronUp />
+            </button>
+            <p className="amount">{amount}</p>
+            <button className="amount-btn">
+                <ChevronDown />
+            </button>
         </div>
     </article>
   )
