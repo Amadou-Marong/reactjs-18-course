186299b6ec437424a09f5efb5fb81c7d927783fe
Amadou-Marong <marongamadou6@gmail.com>
2024-06-26 13:42:02 +0000

    navlinks

--- 12-comfy-store/daisy-store/src/components/Navbar.jsx
+++ 12-comfy-store/daisy-store/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { BsCart3 } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   return (
@@ -8,19 +9,21 @@ const Navbar = () => {
         <div className="navbar align-element">
             <div className="navbar-start">
                 {/* TITLE */}
-                <NavLink to="/" className="hidden lg:flex btn btn-primary 3xl items-center">C</NavLink>
+                <NavLink to="/" className="hidden lg:flex btn btn-primary text-3xl items-center">C</NavLink>
                 {/* DROPDOWN */}
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <FaBarsStaggered />
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52">
-                        nav links
+                        <NavLinks />
                     </ul>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal">nav links</ul>
+                <ul className="menu menu-horizontal">
+                    <NavLinks />
+                </ul>
             </div>
             <div className="navbar-end">
                 <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
