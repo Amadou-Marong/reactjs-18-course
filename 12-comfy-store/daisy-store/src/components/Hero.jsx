8bfb5bdb52b62a657df9091b2cc89830a181a9de
Amadou-Marong <marongamadou6@gmail.com>
2024-07-01 15:43:21 +0000

    hero component

--- 12-comfy-store/daisy-store/src/components/Hero.jsx
+++ 12-comfy-store/daisy-store/src/components/Hero.jsx
@@ -12,7 +12,22 @@ const carouselImages = [
 const Hero = () => {
   return (
     <div className="grid grid-cols-1 lg-grid-cols-2 gap-24 items-center">
-        
+        <div>
+            <h1 className="max-w-2xl text-4xl font-bold tracking-tight">We’re changing the way people shop.</h1>
+            <p className="mt-8 max-w-xl text-lg leading-8">sample Lorem ipsum text</p>
+            <div className="mt-10">
+                <Link to="/products" className="btn btn-primary">Shop Now</Link>
+            </div>
+            <div className="hidden lg:carousel carousel-center p-4 h-[28rem] space-x-4 rounded-box bg-neutral">
+                {carouselImages.map((image, index) => {
+                    return (
+                        <div className="carousel-item" key={index}>
+                            <img src={image} alt={`hero ${index + 1}`} className="rounded-box h-full w-80 object-cover"/>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     </div>
   )
 }
