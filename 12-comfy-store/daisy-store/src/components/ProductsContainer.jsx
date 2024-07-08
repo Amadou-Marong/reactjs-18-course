6a2aeccd9f81723d03bca6df40b8c080c507542e
Amadou-Marong <marongamadou6@gmail.com>
2024-07-08 14:58:26 +0000

    productslist

--- 12-comfy-store/daisy-store/src/components/ProductsContainer.jsx
+++ 12-comfy-store/daisy-store/src/components/ProductsContainer.jsx
@@ -1,6 +1,10 @@
+import ProductsList from "./ProductsList"
+
 const ProductsContainer = () => {
   return (
-    <div>ProductsContainer</div>
+    <div>
+        <ProductsList />
+    </div>
   )
 }
 
