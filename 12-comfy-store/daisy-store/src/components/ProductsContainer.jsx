73f14b7e46154ca2adf8eedb6df6bed7f29b106e
Amadou-Marong <marongamadou6@gmail.com>
2024-07-08 15:19:35 +0000

    products container

--- 12-comfy-store/daisy-store/src/components/ProductsContainer.jsx
+++ 12-comfy-store/daisy-store/src/components/ProductsContainer.jsx
@@ -1,9 +1,30 @@
+import ProductsGrid from "./ProductsGrid"
 import ProductsList from "./ProductsList"
+import { useLoaderData } from "react-router-dom"
+import { useState } from "react"
+import { BsFillGridFill, BsList } from "react-icons/bs"
 
 const ProductsContainer = () => {
+  const { meta } = useLoaderData()
+  console.log(meta);
+  const {total} = meta.pagination
+  const [isGridView, setIsGridView] = useState(true)
   return (
     <div>
-        <ProductsList />
+        {/* <ProductsList /> */}
+        {/* <ProductsGrid /> */}
+        <div className="flex justify-between items-center">
+            <h2 className="text-2xl font-bold text-neutral-content">Products</h2>
+            <div className="flex gap-4">
+                <button onClick={() => setIsGridView(true)} className={`btn ${isGridView ? 'btn-primary' : 'btn-outline'}`}>
+                    <BsFillGridFill />
+                </button>
+                <button onClick={() => setIsGridView(false)} className={`btn ${!isGridView ? 'btn-primary' : 'btn-outline'}`}>
+                    <BsList />
+                </button>
+            </div>
+          </div>
+        {isGridView ? <ProductsGrid /> : <ProductsList />}
     </div>
   )
 }
