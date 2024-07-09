f163367da682bdec6e188a925de9f7d17d93d38b
Amadou-Marong <marongamadou6@gmail.com>
2024-07-09 15:30:51 +0000

    add size prop to forminput

--- 12-comfy-store/daisy-store/src/components/FormInput.jsx
+++ 12-comfy-store/daisy-store/src/components/FormInput.jsx
@@ -1,4 +1,4 @@
-const FormInput = ({label, name, type, defaultValue}) => {
+const FormInput = ({label, name, type, defaultValue, size}) => {
   return (
     <div className="form-control">
       <div className="label">
@@ -8,7 +8,7 @@ const FormInput = ({label, name, type, defaultValue}) => {
         type={type}
         name={name}
         placeholder={defaultValue}
-        className="input input-bordered"
+        className={`input input-bordered ${size}`}
       />
     </div>
   );
