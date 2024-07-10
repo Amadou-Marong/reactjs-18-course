7c25d9bf313e4b36024753518add46c9ef868a53
Amadou-Marong <marongamadou6@gmail.com>
2024-07-10 12:50:09 +0000

    form select component

--- 12-comfy-store/daisy-store/src/components/Filters.jsx
+++ 12-comfy-store/daisy-store/src/components/Filters.jsx
@@ -1,10 +1,16 @@
-import { Form, Link } from "react-router-dom"
+import { Form, Link, useLoaderData } from "react-router-dom"
 import FormInput from "./FormInput"
+import FormSelect from "./FormSelect"
 
 const Filters = () => {
+  const {meta} = useLoaderData()
+  const categories = meta.categories
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+        {/* SEARCH */}
         <FormInput label="search product" name="search" type="search" size="input-sm" />
+        {/* CATEGORIES */}
+        <FormSelect label="category" name="category" size="select-sm" list={categories} />
         <button type="submit" className="btn btn-primary btn-sm">Search</button>
         <Link to="/products" className="btn btn-secondary btn-sm">Reset</Link>
     </Form>    
