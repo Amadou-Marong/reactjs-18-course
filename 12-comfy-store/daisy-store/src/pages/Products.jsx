175e4fb0f31ba3c053d86930994a49a888f7cbac
Amadou-Marong <marongamadou6@gmail.com>
2024-07-11 05:45:58 +0000

    url search params to get search values

--- 12-comfy-store/daisy-store/src/pages/Products.jsx
+++ 12-comfy-store/daisy-store/src/pages/Products.jsx
@@ -3,6 +3,14 @@ import { customFetch } from '../utils'
 
 const url = '/products'
 export const loader = async ({request}) => {
+
+  const params = Object.fromEntries([
+    ...new URLSearchParams(request.url).entries()
+    // ...new URL(request.url).searchParams.entries()
+  ])
+
+  console.log(params);
+
   const response = await customFetch(url)
   const products = await response.data.data
   const meta = await response.data.meta
