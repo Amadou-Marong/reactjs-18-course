8743d4eca30b0ee4925f7351decaa3eab47de8cd
Amadou-Marong <marongamadou6@gmail.com>
2024-07-16 20:54:30 +0000

    added cartTotals component

---
+++ 12-comfy-store/daisy-store/src/components/CartTotals.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux"
+import { formatPrice } from "../utils"
+const CartTotals = () => {
+  return (
+    <div>CartTotals</div>
+  )
+}
+
+export default CartTotals
