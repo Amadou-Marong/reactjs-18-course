3635515e1f3474932b49c5c1206002d98329e400
Amadou-Marong <marongamadou6@gmail.com>
2024-07-19 00:22:09 +0000

    added the cartItem component

--- 12-comfy-store/daisy-store/src/components/CartItem.jsx
+++ 12-comfy-store/daisy-store/src/components/CartItem.jsx
@@ -1,7 +1,20 @@
+import { useDispatch } from "react-redux"
+
 const CartItem = ({cartItem}) => {
-    console.log(cartItem)
+  const {amount, cartID, company, image, price, productColor, productID, title} = cartItem
+  const dispatch = useDispatch()
   return (
-    <div>CartItem</div>
+    <article key={cartID} className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0">
+      <img src={image} alt={title} className="h-24 w-24 object-cover rounded-lg sm:h-32 sm:w-32" />
+      <div className="sm:ml-16 sm:w-48">	
+        <h3>{title}</h3>
+        <h4>{company}</h4>	
+        <h4>{productColor}</h4>
+      </div>
+      <div>
+        
+      </div>
+    </article>
   )
 }
 
