7704c990fd68b105b6d323bb60942f9d8bf2bbda
Amadou-Marong <marongamadou6@gmail.com>
2024-09-02 12:26:30 +0000

    checkout page

--- 12-comfy-store/daisy-store/src/pages/Checkout.jsx
+++ 12-comfy-store/daisy-store/src/pages/Checkout.jsx
@@ -3,10 +3,19 @@ import { CartTotals, CheckoutForm, SectionTitle } from "../components"
 
 
 const Checkout = () => {
-  
+  const cartTotal = useSelector((state) => state.cartState.orderTotal)
+
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your Cart is Empty" />
+  }
   return (
-    <div>Checkout</div>
-  
+    <>
+      <SectionTitle text="Place your order" />
+      <div className='mt-8 grid gap-8  md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
   )
 }
 
