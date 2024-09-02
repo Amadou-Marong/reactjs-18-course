1d3a81203c7ff640eca90702a20ef83a5a97f819
Amadou-Marong <marongamadou6@gmail.com>
2024-09-02 11:13:39 +0000

    guest login

--- 12-comfy-store/daisy-store/src/pages/Login.jsx
+++ 12-comfy-store/daisy-store/src/pages/Login.jsx
@@ -1,6 +1,29 @@
+import { useDispatch } from "react-redux"
 import { FormInput, SubmitBtn } from "../components"
-import { Form, Link } from "react-router-dom"
+import { Form, Link, useNavigate } from "react-router-dom"
+import { customFetch } from "../utils"
+import { toast } from "react-toastify"
+import { loginUser } from "../features/user/userSlice"
 const Login = () => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const loginAsGuest = async () => {
+    try {
+      const response = await customFetch("/auth/local", {
+        identifier: "test@test.com",
+        password: "secret"
+      });
+
+      dispatch(loginUser(response.data))
+      toast.success("Logged in as guest")
+      navigate("/")
+    } catch (error) {
+      console.log(error)
+      toast.error("Failed to login as guest")
+    }
+  }
   
   return (
     <section className="h-screen grid place-items-center">
@@ -11,7 +34,7 @@ const Login = () => {
         <div className="mt-4">
           <SubmitBtn text="Login"/>
         </div>
-        <button className="btn btn-secondary btn-block">guest user</button>
+        <button className="btn btn-secondary btn-block" onClick={loginAsGuest}>guest user</button>
           <p className="text-center">
             Not a member yet? <Link to="/register" className="ml-2 link link-hover link-primary capitalize">Register</Link>
           </p>
