5fc07dc601ef187ddcc1163a2b66cf77e75f81e7
Amadou-Marong <marongamadou6@gmail.com>
2024-09-02 12:38:00 +0000

    checkout action

--- 12-comfy-store/daisy-store/src/App.jsx
+++ 12-comfy-store/daisy-store/src/App.jsx
@@ -4,6 +4,10 @@ import { ErrorElement } from "./components";
 import { loader as landingLoader} from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productsLoader } from "./pages/Products";
+import { loader as checkoutLoader } from "./pages/Checkout";
+import { store } from "./store";
+import { action as checkoutAction } from "./components/CheckoutForm";
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -41,7 +45,9 @@ const router = createBrowserRouter([
       },
       {
         path: 'checkout',
-        element: <Checkout />
+        element: <Checkout />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store)
       }
     ]
   },
--- 12-comfy-store/daisy-store/src/pages/Checkout.jsx
+++ 12-comfy-store/daisy-store/src/pages/Checkout.jsx
@@ -1,6 +1,16 @@
 import { useSelector } from "react-redux"
 import { CartTotals, CheckoutForm, SectionTitle } from "../components"
+import { redirect } from "react-router-dom"
+import { toast } from "react-toastify"
 
+export const loader = (store) => async () => {
+  const user = store.getState().userState.user
+  if (!user) {
+    toast.warn('Please login to place an order')
+    return redirect('/login')
+  }
+  return null
+}
 
 const Checkout = () => {
   const cartTotal = useSelector((state) => state.cartState.orderTotal)
