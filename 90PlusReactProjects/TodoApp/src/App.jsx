f2ae79ae5c657ed1989e90754100b9a22dd3d2b3
Amadou-Marong <marongamadou6@gmail.com>
2025-02-24 22:14:58 +0000

    delete function

--- 90PlusReactProjects/TodoApp/src/App.jsx
+++ 90PlusReactProjects/TodoApp/src/App.jsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 import todos from "./data/todos";
 
 const EditModal = ({ todo, isOpen, onClose, onUpdate }) => {
-  const [updatedTitle, setUpdatedTitle] = useState(todo?.title || "");
-  console.log(todo);
-  
+  const [updatedTitle, setUpdatedTitle] = useState("");
 
-  if(!isOpen) return null
+  useEffect(() => {
+    setUpdatedTitle(todo?.title || "");
+  }, [todo]);
+
+  if (!isOpen) return null;
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    onUpdate(todo?.id, updatedTitle)
-    onClose()
-  }
+    e.preventDefault();
+    onUpdate(todo?.id, updatedTitle);
+
+    onClose();
+  };
 
   return (
     // <div className="fixed inset-0 flex items-center justify-center bg-transparent bg-opacity-30 z-50 backdrop-blur-md">
@@ -30,14 +33,14 @@ const EditModal = ({ todo, isOpen, onClose, onUpdate }) => {
           <div className="flex justify-end gap-3">
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
+              className="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
             >
               Save
             </button>
             <button
               onClick={onClose}
               type="button"
-              className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition"
+              className="cursor-pointer px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition"
             >
               Close
             </button>
@@ -48,74 +51,157 @@ const EditModal = ({ todo, isOpen, onClose, onUpdate }) => {
   );
 };
 
+const ConfirmModal = ({todo, isOpen, onClose, onDelete}) => {
+  
+  if (!isOpen) return null
+  
+  const confirmDelete = () => {
+    onDelete(todo?.id)
+    onClose()
+  }
+
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+        <div className="min-w-96 bg-white shadow-lg p-6 rounded-lg">
+          <h2 className="text-xl font-semibold mb-4">Are you Sure Delete {todo?.title}</h2>     
+            <div className="flex justify-end gap-3">
+              <button
+                onClick={confirmDelete}
+                type="button"
+                className="cursor-pointer px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
+              >
+                Delete
+              </button>
+              <button
+                onClick={onClose}
+                type="button"
+                className="cursor-pointer px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400 transition"
+              >
+                Cancel
+              </button>
+            </div> 
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Todos = ({ myTodos, onOpenModal }) => {
+  const handleOpenModal = (todo) => {
+    onOpenModal(todo);
+  };
+  return (
+    <>
+      <ul>
+        {myTodos.map((todo, index) => (
+          <li key={index} className="p-2">
+            <div className="flex items-center gap-x-2 justify-between">
+              {todo?.id}. {todo?.title}
+              <span>{todo?.completed ? "Done" : "Not Done"}</span>
+              <button
+                onClick={() => handleOpenModal(todo)}
+                className="cursor-pointer px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => handleOpenModal(todo)}
+                className="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
+              >
+                Edit
+              </button>
+              <input type="checkbox" name="" id="" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 function App() {
   const [myTodos, setMyTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedTodo, setSelectedTodo] = useState(null)
+  const [selectedTodo, setSelectedTodo] = useState(null);
 
   useEffect(() => {
     setMyTodos(todos);
-  }, [])
+  }, []);
 
-  
   const addTodo = () => {
     const todo = {
       id: myTodos.length + 1,
       title: newTodo,
-      completed: false
-    }
-    
-    if(!newTodo.trim()) return
+      completed: false,
+    };
+
+    if (!newTodo.trim()) return;
 
     setMyTodos([...myTodos, todo]);
     setNewTodo("");
-  }
+  };
 
   const handleCloseModal = () => {
-    setIsModalOpen(false)
-  }
+    setIsModalOpen(false);
+  };
 
   const handleOpenModal = (todo) => {
-    
-    setSelectedTodo(todo)
-    setIsModalOpen(true)
-  }
+    setSelectedTodo(todo);
+    setIsModalOpen(true);
+  };
 
-  const handleUpdate = (todo, updatedTitle) => {
+  const handleUpdate = (id, updatedTitle) => {
     // console.log(todo, updatedTitle);
-  }
+    setMyTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, title: updatedTitle } : todo
+      )
+    );
+  };
 
+  const handleDelete = (id) => {
+    setMyTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
       <main className="grid place-items-center min-h-screen bg-gray-100">
         <div className="min-w-96 bg-white shadow-lg p-6 rounded-lg">
           <div className="flex items-center gap-x-2">
-          <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} type="text" className="w-full p-2 border border-gray-400"/> <button onClick={addTodo} className="cursor-pointer px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition">Add</button>
+            <input
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+              type="text"
+              className="p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 mb-4"
+            />
+            <button
+              onClick={addTodo}
+              className="p-2 cursor-pointer bg-gray-500 text-white rounded-md hover:bg-gray-600 transition"
+            >
+              Add
+            </button>
           </div>
           <div>
             <h2 className="text-2xl font-bold">My Todos</h2>
-            <ul>
-              {myTodos.map((todo, index) => (
-                <li key={index} className="p-2">
-                  <div className="flex items-center gap-x-2">
-                  {todo.id}. {todo.title} <span>{todo.completed ? "Done" : "Not Done"}</span>
-                  
-                  <button className="cursor-pointer px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition">Delete</button>
-                  <button onClick={() => handleOpenModal(todo)} className="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">Edit</button>
-                  <input type="checkbox" name="" id="" />
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <Todos myTodos={myTodos} onOpenModal={handleOpenModal} />
           </div>
         </div>
-        {<EditModal todo={selectedTodo} isOpen={isModalOpen} onClose={handleCloseModal} onUpdate={handleUpdate}/>}
+        {
+          <EditModal
+            todo={selectedTodo}
+            isOpen={isModalOpen}
+            onClose={handleCloseModal}
+            onUpdate={handleUpdate}
+          />
+        }
+        {
+          <ConfirmModal todo={selectedTodo} isOpen={isModalOpen} onClose={handleCloseModal} onDelete={handleDelete}/>
+        }
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
