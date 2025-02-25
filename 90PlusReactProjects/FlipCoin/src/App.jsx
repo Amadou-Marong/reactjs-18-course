0f896901ab328c358990a7be8dd9e1a6f073ddca
Amadou-Marong <marongamadou6@gmail.com>
2025-02-25 12:45:36 +0000

    added sides count

--- 90PlusReactProjects/FlipCoin/src/App.jsx
+++ 90PlusReactProjects/FlipCoin/src/App.jsx
@@ -18,25 +18,24 @@ function App() {
   
   const handleFlipCoin = () => {
     setIsFlipping(true)
-    const currentSide = Math.floor(Math.random() * coinSides.length)
+    const currentSideIndex = Math.floor(Math.random() * coinSides.length)
     setTimeout(() => {
-      setSide(currentSide)
+      setSide(currentSideIndex)
       setIsFlipping(false)
     }, 2000)
-    // handleSideCount(side)
+
+    setTimeout(() => {
+      handleSideCount(coinSides[currentSideIndex].side)
+    }, 3000)
   }
 
-  // const handleSideCount = (currentSide) => {
-  //   if (currentSide === "head") {
-  //     setHeadCount((prev) => prev += 1)
-  //   } else if (currentSide === "tail") {
-  //     setTailCount((prev) => prev += 1)
-  //   }
-  // }
-
-  // console.log("Heads", headCount);
-  // console.log("Tails", tailCount);
-  
+  const handleSideCount = (currentSide) => {
+    if (currentSide === "head") {
+      setHeadCount((prev) => prev += 1)
+    } else if (currentSide === "tail") {
+      setTailCount((prev) => prev += 1)
+    }
+  }
 
   return (
     <>
@@ -57,6 +56,14 @@ function App() {
               />
             </AnimatePresence>
               {/* <img className="w-full" src={`${coin[side]?.imgSrc}`} alt="" /> */}
+          </div>
+          <div className="grid my-4 grid-cols-2 gap-2">
+              <div className="shadow-md p-2 border-gray-200 bg-gray-200 border-md">
+                  <h3 className="text-lg font-bold">Heads: {headCount}</h3>
+              </div>
+              <div className="shadow-md p-2 border-gray-200 bg-gray-200 border-md">
+                  <h3 className="text-lg font-bold">Tails: {tailCount}</h3>
+              </div>
           </div>
           <div className="mt-4 flex justify-center gap-4">
             <button disabled={isFlipping} onClick={handleFlipCoin} className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition cursor-pointer">{isFlipping ? "Flipping" : "Flip Coins"}</button>
