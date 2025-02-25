beb7e23ca6b4359d3a703d841f24bf75819edee8
Amadou-Marong <marongamadou6@gmail.com>
2025-02-25 12:51:36 +0000

    added reset functionality

--- 90PlusReactProjects/FlipCoin/src/App.jsx
+++ 90PlusReactProjects/FlipCoin/src/App.jsx
@@ -16,6 +16,14 @@ function App() {
 
   // console.log(coin[0]?.imgSrc);
   
+  const handleSideCount = (currentSide) => {
+    if (currentSide === "head") {
+      setHeadCount((prev) => prev += 1)
+    } else if (currentSide === "tail") {
+      setTailCount((prev) => prev += 1)
+    }
+  }
+
   const handleFlipCoin = () => {
     setIsFlipping(true)
     const currentSideIndex = Math.floor(Math.random() * coinSides.length)
@@ -29,12 +37,10 @@ function App() {
     }, 3000)
   }
 
-  const handleSideCount = (currentSide) => {
-    if (currentSide === "head") {
-      setHeadCount((prev) => prev += 1)
-    } else if (currentSide === "tail") {
-      setTailCount((prev) => prev += 1)
-    }
+  const handleReset = () => {
+    setSide(0)
+    setHeadCount(0)
+    setTailCount(0)
   }
 
   return (
@@ -66,6 +72,7 @@ function App() {
               </div>
           </div>
           <div className="mt-4 flex justify-center gap-4">
+            <button disabled={isFlipping} onClick={handleReset} className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition cursor-pointer">Reset</button>
             <button disabled={isFlipping} onClick={handleFlipCoin} className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition cursor-pointer">{isFlipping ? "Flipping" : "Flip Coins"}</button>
           </div>
         </div>
