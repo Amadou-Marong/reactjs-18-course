82e9d370cc70f524b538dd6721a051710d5e9dc1
Amadou-Marong <marongamadou6@gmail.com>
2025-02-28 11:25:11 +0000

    BMI Calculator

--- 90PlusReactProjects/BMICalculator/src/App.jsx
+++ 90PlusReactProjects/BMICalculator/src/App.jsx
@@ -1,29 +1,32 @@
 function App() {
-
   return (
     <>
       <main>
         <div className="container center">
-          <form className="mt-4 form">
-            <h1 className="title text-white text-center">BMI Calculator</h1>
-            <div className="my-4">
-              <label className="text-white block">Weight</label>
-              <input type="text" className="w-full"/>
-            </div>
-            <div className="my-4">
-              <label className="text-white block">Height</label>
-              <input type="text" className="w-full"/>
-            </div>
-            <button type="submit" className="btn btn-primary">Calculate</button>
-          </form>
+          <div className="flex gap-4">
+            <form className="mt-4 form">
+              <h1 className="title text-white text-center">BMI Calculator</h1>
+              <div className="my-4">
+                <label className="text-white block">Weight(kg)</label>
+                <input type="text" className="w-full" />
+              </div>
+              <div className="my-4">
+                <label className="text-white block">Height(cm)</label>
+                <input type="text" className="w-full" />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Calculate
+              </button>
+            </form>
 
-          <div className="box mt-4">
-            <h3 className="text-white text-center">Result</h3>
+            <div className="box mt-4">
+              <h3 className="title text-white text-center">Result</h3>
+            </div>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
